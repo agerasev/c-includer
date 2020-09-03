d980297601e340447104e6af7af516ac6e32a298
Alexey Gerasev <alexey.gerasev@gmail.com>
2020-09-03 10:26:39 +0700

    Import new version from hypertrace project

---
+++ README.md
@@ -0,0 +1,7 @@
+# C Includer
+
+`#include` directives resolver for C-like sources.
+
+## Usage
+
+See usage examples in [src/test.cpp](src/test.cpp) file.
---
+++ src/test.cpp
@@ -0,0 +1,248 @@
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+#include <includer.hpp>
+
+
+TEST_CASE("C Includer", "[include]") {
+    SECTION("No include") {
+        std::string main = "\
+int main() {\n\
+    return 0;\n\
+}\n\
+";
+        includer includer(
+            "main.c",
+            std::list<std::string>{},
+            std::map<std::string, std::string>{
+                std::make_pair(std::string("main.c"), main)
+            }
+        );
+
+        REQUIRE(includer.include());
+        REQUIRE(includer.data() == main);
+    }
+
+    SECTION("Single include") {
+        std::string main = "\
+#include <header.h>\n\
+#include <header.h>\n\
+// Main function\n\
+int main() {\n\
+    return RET_CODE;\n\
+}\n\
+";
+        std::string header = "\
+#pragma once\n\
+// Return code\n\
+static const int RET_CODE = 0;\n\
+";
+
+        std::string result = "\
+\n\
+// Return code\n\
+static const int RET_CODE = 0;\n\
+\n\
+\n\
+// Main function\n\
+int main() {\n\
+    return RET_CODE;\n\
+}\n\
+";
+        includer includer(
+            "main.c",
+            std::list<std::string>{},
+            std::map<std::string, std::string>{
+                std::make_pair(std::string("main.c"), main),
+                std::make_pair(std::string("header.h"), header)
+            }
+        );
+
+        REQUIRE(includer.include());
+        REQUIRE(includer.data() == result);
+    }
+
+    SECTION("Recursion") {
+        std::string first = "\
+#pragma once\n\
+#include <second.h>\n\
+";
+        std::string second = "\
+#pragma once\n\
+#include <first.h>\n\
+";
+
+        includer includer(
+            "first.h",
+            std::list<std::string>{},
+            std::map<std::string, std::string>{
+                std::make_pair(std::string("first.h"), first),
+                std::make_pair(std::string("second.h"), second)
+            }
+        );
+
+        REQUIRE(includer.include());
+        REQUIRE(includer.data() == "\n\n\n\n");
+    }
+
+    SECTION("Multiple headers") {
+        std::string main = "\
+#include <h02.h>\n\
+#include <h01.h>\n\
+";
+        std::string h01 = "\
+#pragma once\n\
+#include <h02.h>\n\
+h01\n\
+";
+        std::string h02 = "\
+#pragma once\n\
+#include <h01.h>\n\
+h02\n\
+";
+
+        includer includer(
+            "main.c",
+            std::list<std::string>{},
+            std::map<std::string, std::string>{
+                std::make_pair(std::string("main.c"), main),
+                std::make_pair(std::string("h01.h"), h01),
+                std::make_pair(std::string("h02.h"), h02)
+            }
+        );
+
+        REQUIRE(includer.include());
+        REQUIRE(includer.data() == "\n\n\nh01\n\nh02\n\n\n");
+    }
+
+    SECTION("Multiple headers") {
+        std::string main = "\
+0\n\
+1\n\
+2\n\
+#include <h01.h>\n\
+9\n\
+10\n\
+#include <h03.h>\n\
+15\n\
+16\n\
+";
+        std::string h01 = "\
+3\n\
+#include <h02.h>\n\
+7\n\
+";
+        std::string h02 = "\
+4\n\
+5\n\
+";
+        std::string h03 = "\
+11\n\
+12\n\
+13\n\
+";
+
+        includer includer(
+            "main.c",
+            std::list<std::string>{},
+            std::map<std::string, std::string>{
+                std::make_pair(std::string("main.c"), main),
+                std::make_pair(std::string("h01.h"), h01),
+                std::make_pair(std::string("h02.h"), h02),
+                std::make_pair(std::string("h03.h"), h03)
+            }
+        );
+
+        REQUIRE(includer.include());
+        std::list<std::string> lines;
+        std::string accum;
+        for (char c : includer.data()) {
+            if (c == '\n') {
+                lines.push_back(accum);
+                accum.clear();
+            } else {
+                accum += c;
+            }
+        }
+        lines.push_back(accum);
+        int i = 0;
+        for (const std::string &line : lines) {
+            if (line.size() > 0) {
+                REQUIRE(std::stoi(line) == i);
+            }
+            i += 1;
+        }
+    }
+
+    SECTION("Multiple headers") {
+        std::string main = "\
+00\n\
+01\n\
+02\n\
+#include <h01.h>\n\
+04\n\
+05\n\
+#include <h03.h>\n\
+07\n\
+08\n\
+";
+        std::string h01 = "\
+10\n\
+#include <h02.h>\n\
+12\n\
+";
+        std::string h02 = "\
+20\n\
+21\n\
+";
+        std::string h03 = "\
+30\n\
+31\n\
+32\n\
+";
+
+        includer includer(
+            "main.c",
+            std::list<std::string>{},
+            std::map<std::string, std::string>{
+                std::make_pair(std::string("main.c"), main),
+                std::make_pair(std::string("h01.h"), h01),
+                std::make_pair(std::string("h02.h"), h02),
+                std::make_pair(std::string("h03.h"), h03)
+            }
+        );
+
+        REQUIRE(includer.include());
+        std::list<std::string> lines;
+        std::string accum;
+        for (char c : includer.data()) {
+            if (c == '\n') {
+                lines.push_back(accum);
+                accum.clear();
+            } else {
+                accum += c;
+            }
+        }
+        assert(accum.size() == 0);
+        int gpos = 0;
+        for (const std::string &line : lines) {
+            std::string name;
+            int lpos;
+            REQUIRE(includer.locate(gpos, &name, &lpos));
+            if (line.size() > 0) {
+                REQUIRE(line.size() == 2);
+                char f = line[0];
+                int l = std::stoi(line.substr(1, 1));
+                if (f == '0') {
+                    REQUIRE(name == "main.c");
+                } else {
+                    std::string hn = "h0x.h";
+                    hn[2] = f;
+                    REQUIRE(name == hn);
+                }
+                REQUIRE(lpos == l);
+            }
+            gpos += 1;
+        }
+        REQUIRE(!includer.locate(gpos, nullptr, nullptr));
+    }
+};
